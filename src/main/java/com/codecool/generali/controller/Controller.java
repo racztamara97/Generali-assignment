c4d6253624ac52e9ae977d78b830124dd982ed30
racztamara97 <racztamara97@gmail.com>
2019-01-30 09:57:46 +0100

    /index route created, call happens if reload the page

---
+++ src/main/java/com/codecool/generali/controller/Controller.java
@@ -0,0 +1,44 @@
+package com.codecool.generali.controller;
+
+import com.codecool.generali.model.Call;
+import com.codecool.generali.repository.CallRepository;
+import com.codecool.generali.service.CallService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+@org.springframework.stereotype.Controller
+@Scope("session")
+public class Controller {
+
+    @Autowired
+    private CallService callService;
+
+    @Autowired
+    private CallRepository callRepository;
+
+    @ModelAttribute("call")
+    public Call createCall() {
+        return new Call();
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Call newCall = callService.createNewCall();
+        model.addAttribute("newCall", newCall);
+        return "index";
+    }
+
+
+}
