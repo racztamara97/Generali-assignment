7a32bab443cf523bd8fc42095183d8d331ad467a
racztamara97 <racztamara97@gmail.com>
2019-01-30 09:58:29 +0100

    CallRepository initialized

---
+++ src/main/java/com/codecool/generali/repository/CallRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.generali.repository;
+
+import com.codecool.generali.model.Call;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CallRepository extends JpaRepository<Call, String> {
+}
