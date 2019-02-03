263fd22bfaf44db57bd0f6b9eab65167b8f4f48f
racztamara97 <racztamara97@gmail.com>
2019-02-03 21:47:26 +0100

    findAll added

--- src/main/java/com/codecool/generali/repository/CallRepository.java
+++ src/main/java/com/codecool/generali/repository/CallRepository.java
@@ -3,5 +3,9 @@ package com.codecool.generali.repository;
 import com.codecool.generali.model.Call;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CallRepository extends JpaRepository<Call, String> {
+
+    List<Call> findAll();
 }
